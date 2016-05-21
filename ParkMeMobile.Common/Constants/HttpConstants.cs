0704bd8d33339b49ad796d0aa728ead888a3cf98
claudiu codoban <claudiu.codoban@deventure.co>
2016-05-22 02:01:42 +0300

    Added http client for doing a get request

---
+++ ParkMeMobile.Common/Constants/HttpConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace ParkMeMobile.Common.Constants
+{
+    public static class HttpConstants
+    {
+        public const string ENDPOINT_GET_ALL_PARKS = "http://172.16.3.222:1234/api1/values/GetAnonymous";
+    }
+}
---
+++ ParkMeMobile.Common/Http/HttpClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ParkMeMobile.Common.Http
+{
+    public static class HttpClient
+    {
+        public static async Task<T> Get<T>(string url)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            using (var response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response:
+                using (var stream = response.GetResponseStream())
+                {
+                    return JsonConvert.DeserializeObject<T>(stream.ToString());
+                }
+            }
+        }
+    }
+}
