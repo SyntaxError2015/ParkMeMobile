9ce8bb3a7ba426f3c4f86de556279217fa32d107
claudiu codoban <claudiu.codoban@deventure.co>
2016-05-22 01:28:10 +0300

    Added models for json serialization

--- ParkMeMobile.Common/Models/Slot.cs
+++ ParkMeMobile.Common/Models/Slot.cs
@@ -1,7 +1,19 @@
-﻿namespace ParkMeMobile.Common.Models
+﻿using Newtonsoft.Json;
+
+namespace ParkMeMobile.Common.Models
 {
     public class Slot
     {
+        [JsonProperty("id")]
+        public string Id { get; set; }
 
+        [JsonProperty("isOccupied")]
+        public string IsOccupied { get; set; }
+
+        [JsonProperty("park")]
+        public Park Park { get; set; }
+
+        [JsonProperty("position")]
+        public Point Position { get; set; }
     }
 }
