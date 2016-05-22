69a3aac542102049a98fd3d80273bbb6ab2e874a
claudiu codoban <claudiu.codoban@deventure.co>
2016-05-22 07:55:08 +0300

    Added maps camera manipulation and integrated the polling service. Still need to do the update logic

--- ParkMeMobile.Android/UI/Activities/MainActivity.cs
+++ ParkMeMobile.Android/UI/Activities/MainActivity.cs
@@ -1,56 +1,107 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
 using Android.Locations;
 using Android.OS;
-using Android.Util;
+using ParkMeMobile.Common.Models;
+using ParkMeMobile.Common.Polling;
 
 namespace ParkMeMobile.Android.UI.Activities
 {
     [Activity(Label = "ParkMe!", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, ILocationListener
     {
         private MapFragment mMapFragment;
         private LocationManager mLocationManager;
         private string mLocationProvider;
+        private Location mCurrentLocation;
+        private Address mCurrentAddress;
+        private GoogleMap mMap;
+
+        private PollingService<Park> mPollingService;
+
+        #region Lifecycle
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
+            mPollingService = new PollingService<Park>(UpdateUI);
+
             InitializeLocationManager();
             InitializeMaps();
         }
 
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            mLocationManager.RequestLocationUpdates(mLocationProvider, 0, 0, this);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            mLocationManager.RemoveUpdates(this);
+        }
+
+        #endregion
+
+        #region Data management
+
+        private void UpdateUI(Park park)
+        {
+            MoveCameraOnCurrentPosition();
+        }
+
+        #endregion
+
+        #region Maps management
+
         private void InitializeMaps()
         {
-            var location = new LatLng(50.897778, 3.013333);
-            var builder = CameraPosition.InvokeBuilder();
-            builder.Target(location);
-            builder.Zoom(18);
-            builder.Bearing(155);
-            builder.Tilt(65);
-            var cameraPosition = builder.Build();
-            var cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
-
             mMapFragment = (MapFragment) FragmentManager.FindFragmentById(Resource.Id.map_frag);
-            var map = mMapFragment.Map;
+            mMap = mMapFragment.Map;
 
-            if (map == null)
+            if (mMap == null)
             {
                 return;
             }
 
-            map.MapType = GoogleMap.MapTypeNormal;
-            map.UiSettings.ZoomControlsEnabled = true;
-            map.UiSettings.ZoomGesturesEnabled = true;
-            map.UiSettings.CompassEnabled = true;
-            map.UiSettings.MyLocationButtonEnabled = true;
+            mMap.MapType = GoogleMap.MapTypeNormal;
+            mMap.UiSettings.ZoomControlsEnabled = true;
+            mMap.UiSettings.ZoomGesturesEnabled = true;
+            mMap.UiSettings.CompassEnabled = true;
+            mMap.UiSettings.MyLocationButtonEnabled = true;
+        }
 
-            map.MoveCamera(cameraUpdate);
+        private void MoveCameraOnCurrentPosition()
+        {
+            var location = mCurrentAddress != null
+                               ? new LatLng(mCurrentAddress.Latitude, mCurrentAddress.Longitude)
+                               : new LatLng(mCurrentLocation.Latitude, mCurrentLocation.Longitude);
+
+            var locationBuilder = CameraPosition.InvokeBuilder();
+            locationBuilder.Target(location);
+
+            var position = CameraUpdateFactory.NewCameraPosition(locationBuilder.Build());
+            mMap.MoveCamera(position);
+        }
+
+        #endregion
+
+        #region Location management
+
+        private async Task GetCurrentAddress()
+        {
+            var geocoder = new Geocoder(this);
+
+            var addressList = await geocoder.GetFromLocationAsync(mCurrentLocation.Latitude, mCurrentLocation.Longitude, 10);
+
+            mCurrentAddress = addressList.FirstOrDefault();
         }
 
         private void InitializeLocationManager()
@@ -66,5 +117,30 @@ namespace ParkMeMobile.Android.UI.Activities
 
             mLocationProvider = acceptableLocationProviders.Any() ? acceptableLocationProviders.First() : string.Empty;
         }
+
+        public async void OnLocationChanged(Location location)
+        {
+            if (location == null)
+            {
+                return;
+            }
+
+            mCurrentLocation = location;
+            await GetCurrentAddress();
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+        }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+        }
+
+        #endregion
     }
 }
